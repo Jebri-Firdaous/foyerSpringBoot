f0b4eff1a0022f1ca8095481e7ea6afb965513fb
Jebri-Firdaous <jebrifirdaous0@gmail.com>
2025-01-07 17:32:34 +0100

    initial commit

---
+++ src/test/java/tn/esprit/foyerspringboot/FoyerSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.foyerspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoyerSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
